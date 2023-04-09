b8c82934c9463c7f721c13e3f4c7f162f938cf3e
DaftenP <bjeeyong99@gmail.com>
2023-04-09 21:41:11 +0900

    to silence sound containing specific words

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import io
 from google.cloud import speech_v1p1beta1 as speech
+from pydub import AudioSegment
+
+# audio = AudioSegment.from_file("./test/test_audio.wav")
+#
+# # Resample the audio to a higher sample rate (e.g., 48000 Hz)
+# audio = audio.set_frame_rate(48000)
+#
+# # Export the resampled audio to a new file
+# audio.export("./test/test_audio.wav", format="wav")
+
+def silent(timestamps, audiofile):
+    audio_input = AudioSegment.from_file(audiofile, format="wav")
+    for start, end in timestamps:
+        segment_to_silence = AudioSegment.silent(duration=(end - start))
+        audio_input = audio_input[:start] + segment_to_silence + audio_input[end:]
+    audio_input.export(audiofile[:-4]+'_output.wav', format="wav")
+
 
 client = speech.SpeechClient()
 
@@ -13,20 +30,22 @@ audio = speech.RecognitionAudio(content=content)
 config = speech.RecognitionConfig(
     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
     language_code="ko-KR",
+    sample_rate_hertz=48000,
     enable_word_time_offsets=True,
 )
 
 response = client.recognize(config=config, audio=audio)
 
 # word for filtering
-filtering_word = ["무궁화", "동해"]
+filtering_word = ["무궁화", "화려강산"]
+timestamps = []
 
 for result in response.results:
     alternative = result.alternatives[0]
 
-    # whole script of stt
+    # whole scripts of stt result
     print(u"Transcript: {}".format(alternative.transcript))
-    print(f'example for word : {alternative.words[2]}')
+    print(f'example of word : {alternative.words[1]}')
     print("Word level time offsets:")
 
     for word in alternative.words:
@@ -34,5 +53,9 @@ for result in response.results:
 
         # this point is about conversion.
         if word.word in filtering_word:
-            print("flag")
-
+            start = str(word.start_time).split(':')
+            end = str(word.end_time).split(':')
+            timestamps.append([int(int(start[0])*3600000+int(start[1])*60000+float(start[2])*1000),
+                               int(int(end[0]) * 3600000 + int(end[1]) * 60000 + float(end[2])*1000)])
+silent(timestamps, "./test/test_audio.wav")
+print(timestamps)
--- test.py
+++ test.py
@@ -1,66 +1,11 @@
-from google.cloud import speech
-from pydub import AudioSegment
-import numpy as np
-import io
+import kaldi
+model = kaldi.nnet3.NnetSimpleLoopedComputationOptions()
+fst = fst.ReadFstKaldi("path/to/fst.txt")
+decoder = decoder.Decoder(fst, model)
 
+# Set up the decoder
+decodable = online2.WavOnlineMatrixFeature(matrix.FloatMatrix())
+decoder.InitDecoding()
 
-def sample_recognize(local_file_path):
-    client = speech.SpeechClient()
-
-    language_code = "ko-KR"
-    sample_rate_hertz = 44100
-
-    encoding = speech.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
-    config = speech.RecognitionConfig(
-        sample_rate_hertz=sample_rate_hertz,
-        encoding=encoding,
-        language_code=language_code,
-        enable_word_time_offsets=True,
-        use_enhanced=True
-    )
-
-    with io.open(local_file_path, "rb") as f:
-        content = f.read()
-
-    audio = speech.RecognitionAudio(content=content)
-
-    response = client.recognize(config=config, audio=audio)
-
-    timeline, swear_timeline, words = [], [], []
-
-    sound = AudioSegment.from_file('./test/test_audio.wav', format='wav')
-
-    # Create beep Sound
-    def create_beep(duration):
-        sps = 44100
-        freq_hz = 1000.0
-        vol = 0.5
-
-        esm = np.arange(duration / 1000 * sps)
-        wf = np.sin(2 * np.pi * esm * freq_hz / sps)
-        wf_quiet = wf * vol
-        wf_int = np.int16(wf_quiet * 32767)
-
-        beep = AudioSegment(
-            wf_int.tobytes(),
-            frame_rate=sps,
-            sample_width=wf_int.dtype.itemsize,
-            channels=1
-        )
-
-        return beep
-
-    beep = create_beep(duration=1000)
-
-    # Overlay Partially
-    i = 0
-    mixed = sound.overlay(beep, position=swear_timeline[i][0], gain_during_overlay=-20)
-
-    # Result
-    mixed_final = sound
-
-    for i in range(len(swear_timeline)):
-        beep = create_beep(duration=swear_timeline[i][1] - swear_timeline[i][0])
-        mixed_final = mixed_final.overlay(beep, position=swear_timeline[i][0], gain_during_overlay=-20)
-
-    mixed_final.export('./test/output.wav', format='wav')
+# Decode the audio
+result = decoder.DecodeSimple(decodable)
